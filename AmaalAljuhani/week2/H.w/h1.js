56caede6e11bb3789ad60c0fd976042f5925924e
amaal aljuhani <50740572+AmaalAljuhani@users.noreply.github.com>
2019-10-26 14:32:13 +0300

    Add files via upload

--- AmaalAljuhani/week2/H.w/h1.js
+++ AmaalAljuhani/week2/H.w/h1.js
@@ -1,8 +1,8 @@
 var i;
 //function print(){
 for ( i = 99; i >= 2; i--) {
-  console.log(i+"bottles of beer on the wall, " + i+ "bottles of beer.");           
-  console.log("Take one down and pass it around, " + (i - 1) + "bottles of beer on the wall.");
+  console.log(i+" bottles of beer on the wall, " +i+ " bottles of beer.");           
+  console.log("Take one down and pass it around, " + (i - 1) + " bottles of beer on the wall.");
 }
 
 console.log("No more bottles of beer on the wall, no more bottles of beer .Go to the store and buy some more, 99 bottles of beer on the wall.");
